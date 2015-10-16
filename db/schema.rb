2f6d8d5b72e7db4d4db42fef941ed990208f5a7f
Garrett Dycus <gjdycus@gmail.com>
2015-10-16 07:52:21 -0700

    Added unique index to username column of users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151013162204) do
+ActiveRecord::Schema.define(version: 20151016145007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,5 +37,6 @@ ActiveRecord::Schema.define(version: 20151013162204) do
   end
 
   add_index "users", ["session_token"], name: "index_users_on_session_token", unique: true, using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
