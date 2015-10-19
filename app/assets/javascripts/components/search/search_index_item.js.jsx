c2e6d826b863b8bdf0c5f706611008e15d931e91
Garrett Dycus <gjdycus@gmail.com>
2015-10-19 11:49:17 -0700

    Replaced navigateToUserProfile with href

--- app/assets/javascripts/components/search/search_index_item.js.jsx
+++ app/assets/javascripts/components/search/search_index_item.js.jsx
@@ -1,11 +1,8 @@
 var SearchIndexItem = React.createClass({
-  navigateToUserProfile: function (userId) {
-    window.location.href = "#/users/" + userId;
-  },
   render: function () {
     return (
       <li>
-        <a onClick={this.navigateToUserProfile.bind(null, this.props.result.id)}>{this.props.result.username}</a>
+        <a href={"#/users/" + this.props.result.id}>{this.props.result.username}</a>
       </li>
     );
   }
